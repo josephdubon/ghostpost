5df93fb81ac6aef7f629a9181e6008ad015466d7
Joseph Dubon <josephdubon@pm.me>
2021-02-21 14:42:28 -0800

    Created model for posts.

--- ghostpost_app/models.py
+++ ghostpost_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    text = models.CharField(max_length=100)
+    # good practice to start with default=0 for IntegerField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    # dunder method to make admin more readable
+    def __str__(self):
+        return self.text
