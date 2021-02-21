43a54d6ff1a22499fa05ae0863d00cad48512d7e
Joseph Dubon <josephdubon@pm.me>
2021-02-21 14:43:19 -0800

    Created index_view and like_view.

--- ghostpost_app/views.py
+++ ghostpost_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ghostpost_app.models import Post
 
-# Create your views here.
+
+def index_view(request):
+    name = 'Ghost Post'
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'app_name': name, 'posts': posts})
+
+
+def like_view(request, post_id):
+    # filter id will bring a query -> using .first gives access to first item
+    post = Post.objects.filter(id=post_id).first()
+    post.likes += 1
+    # must run .save() to save to database
+    post.save()
+    return redirect('/')
+
+
+def dislike_view(request, post_id):
+    pass
