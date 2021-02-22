0a4fec018f2dbfc60c24bcc909e0d7ecbc1e492c
Joseph Dubon <josephdubon@pm.me>
2021-02-21 18:20:14 -0800

    Added drop down

--- ghostpost_app/forms.py
+++ ghostpost_app/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 class CreatePostForm(forms.Form):
     text = forms.CharField(max_length=100)
+    is_roast = forms.ChoiceField(choices=[[0, 'Roast'], [1, 'Boast']])
     # good practice to start with default=0 for IntegerField()
     boast = forms.IntegerField()
     roast = forms.IntegerField()
