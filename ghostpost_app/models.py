beffc7a2b9a0b41665528653c15022fd7e008723
Joseph Dubon <josephdubon@pm.me>
2021-02-21 16:55:30 -0800

    Corrected created_at with (auto_now_add=True) as parameter

--- ghostpost_app/models.py
+++ ghostpost_app/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     # good practice to start with default=0 for IntegerField()
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     # dunder method to make admin more readable
     def __str__(self):
