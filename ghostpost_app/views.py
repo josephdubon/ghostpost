c066a6c2cd6aae77541c41f10b44790f11db87d9
Joseph Dubon <josephdubon@pm.me>
2021-02-21 16:31:26 -0800

    Created dislike_view

--- ghostpost_app/views.py
+++ ghostpost_app/views.py
@@ -1,5 +1,7 @@
+from django.utils import timezone
 from django.shortcuts import render, redirect
 from ghostpost_app.models import Post
+from ghostpost_app.forms import CreatePostForm
 
 
 def index_view(request):
@@ -9,13 +11,45 @@ def index_view(request):
 
 
 def like_view(request, post_id):
-    # filter id will bring a query -> using .first gives access to first item
+    # get post
     post = Post.objects.filter(id=post_id).first()
+    # add like
     post.likes += 1
     # must run .save() to save to database
     post.save()
+    # redirect home for good ux
     return redirect('/')
 
 
 def dislike_view(request, post_id):
-    pass
+    # get post
+    post = Post.objects.filter(id=post_id).first()
+    # add dislike
+    post.dislikes += 1
+    # must run .save() to save to database
+    post.save()
+    # redirect home for good ux
+    return redirect('/')
+
+
+def create_post(request):
+    context = {}
+
+    if request.method == "POST":
+        form = CreatePostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Post.objects.create(
+                likes=data['likes'],
+                dislikes=data['dislikes'],
+                created_at=timezone.now,
+            )
+            return redirect('/')
+
+    form = CreatePostForm()
+    context.update({'form': form})
+    return render(
+        request,
+        'generic_form.html',
+        context
+    )
