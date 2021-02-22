7a55eb0188a8b654eb4d51a9e168842a8e9a24ac
Joseph Dubon <josephdubon@pm.me>
2021-02-21 20:48:56 -0800

    Updated selection wording on dropdown for GHOSTPOST_CHOICES, removed old comment.

--- ghostpost_app/forms.py
+++ ghostpost_app/forms.py
@@ -3,10 +3,9 @@ from django import forms
 
 class CreatePostForm(forms.Form):
     GHOSTPOST_CHOICES = (
-        ('False', 'Boasts'),
-        ('True', 'Roasts')
+        ('True', 'Yes, Roast'),
+        ('False', 'No, A Boast!'),
     )
 
     text = forms.CharField(max_length=100)
     is_roast = forms.ChoiceField(choices=GHOSTPOST_CHOICES)
-    # good practice to start with default=0 for IntegerField()
